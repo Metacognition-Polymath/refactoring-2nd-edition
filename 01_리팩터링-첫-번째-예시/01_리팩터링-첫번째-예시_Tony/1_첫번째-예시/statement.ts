f892cb98868c3dcfe3568dc61d6169652c959692
Tony <gth1123@naver.com>
2021-11-15 23:46:59 +0900

    Tony/1 6 (#3)
    * chore : 서문 정리 중
    
    * style : CLI환경에서 폴더 접근하기 쉽게 이름 변경
    
    * Tony : 1-4. switch문 분리, 변수명 명확하게 변경
    
    * Tony : 1-4. switch문 분리, 변수명 명확하게 변경(readme)
    
    * plays 우변 함수로 분리, 근데 잘 납득 안됨
    
    * 1-4. 적립 포인트 계산 코드 추출하기
    
    * 1-4. format변수 제거(함수로 분리)
    
    * 1-4. volumeCredit, totalAmount 분리
    
    * 1-5. 중간 점검: 난무하는 중첩 함수
    
    * 1-6. 출력코드 분리
    
    * 1-6. enrichPerformance 함수
    
    * 1-6. 타입 undefined 경우 제거 및 불필요 코드 및 주석 제거
    
    * 1-6. playFor()함수를 statement()로 옮기기
    
    * 1-6. amountFor 옮기기
    
    * 1-6. volumeCreditsFor 옮기기
    
    * 1-6. 총합 구하는 부분 옮기기(totalAmount, totalVolumeCredits)
    
    * 1-6. 반복문(for)을 파이프라인(reducer)로 바꾸기
    
    * 1-6. 계산 단계와 포맷팅 단계 분리하기(final)

--- 01_리팩터링-첫-번째-예시/01_리팩터링-첫번째-예시_Tony/1_첫번째-예시/statement.ts
+++ 01_리팩터링-첫-번째-예시/01_리팩터링-첫번째-예시_Tony/1_첫번째-예시/statement.ts
@@ -1,96 +1,99 @@
-// 1. 첫 번째 예시
-const playData: Plays = {
-  hamlet: { name: 'Hamlet', type: 'tragedy' },
-  asLike: { name: 'As You Like It', type: 'comedy' },
-  othello: { name: 'Othello', type: 'tragedy' },
-};
+{
+  // 1. 첫 번째 예시
+  const playData: Plays = {
+    hamlet: { name: 'Hamlet', type: 'tragedy' },
+    asLike: { name: 'As You Like It', type: 'comedy' },
+    othello: { name: 'Othello', type: 'tragedy' },
+  };
 
-const invoiceData: Invoice = {
-  customer: 'BigCo',
-  performances: [
-    {
-      playID: 'hamlet',
-      audience: 55,
-    },
-    {
-      playID: 'asLike',
-      audience: 35,
-    },
-    {
-      playID: 'othello',
-      audience: 40,
-    },
-  ],
-};
+  const invoiceData: Invoice = {
+    customer: 'BigCo',
+    performances: [
+      {
+        playID: 'hamlet',
+        audience: 55,
+      },
+      {
+        playID: 'asLike',
+        audience: 35,
+      },
+      {
+        playID: 'othello',
+        audience: 40,
+      },
+    ],
+  };
 
-// 1.1 공연료 청구서를 출력하는 함수
-type Invoice = {
-  customer: string;
-  performances: {
-    playID: 'hamlet' | 'asLike' | 'othello';
-    audience: number;
-  }[];
-};
+  // 1.1 공연료 청구서를 출력하는 함수
+  type Invoice = {
+    customer: string;
+    performances: {
+      playID: 'hamlet' | 'asLike' | 'othello';
+      audience: number;
+    }[];
+  };
 
-type PlayDetail = {
-  name: string;
-  type: 'tragedy' | 'comedy';
-};
+  type PlayDetail = {
+    name: string;
+    type: 'tragedy' | 'comedy';
+  };
 
-type Plays = {
-  hamlet: PlayDetail;
-  asLike: PlayDetail;
-  othello: PlayDetail;
-};
+  type Plays = {
+    hamlet: PlayDetail;
+    asLike: PlayDetail;
+    othello: PlayDetail;
+  };
 
-function statement(invoice: Invoice, plays: Plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
-  //   console.log(format(10000)); // $10,000.00 : string
+  function statement(invoice: Invoice, plays: Plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+    const format = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format;
+    //   console.log(format(10000)); // $10,000.00 : string
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0; // 총액
+    for (let perf of invoice.performances) {
+      const play = plays[perf.playID];
+      let thisAmount = 0; // 총액
 
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
+      switch (play.type) {
+        case 'tragedy':
+          thisAmount = 40000;
+          if (perf.audience > 30) {
+            thisAmount += 1000 * (perf.audience - 30);
+          }
+          break;
+        case 'comedy':
+          thisAmount = 30000;
+          if (perf.audience > 20) {
+            thisAmount += 10000 + 500 * (perf.audience - 20);
+          }
+          thisAmount += 300 * perf.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${play.type}`);
+      }
+
+      // 포인트 적립
+      volumeCredits += Math.max(perf.audience - 30, 0); // 음수 일 경우 포인트는 0점이 적립
+      // 희극 관객 5명 마다 추가 포인트를 제공
+      if (play.type === 'comedy')
+        volumeCredits += Math.floor(perf.audience / 5); // 소수값 버림
+      // 청구 내역을 출력
+      result += `${play.name}: ${format(thisAmount / 100)} (${
+        perf.audience
+      }석)\n`;
+      totalAmount += thisAmount;
     }
 
-    // 포인트 적립
-    volumeCredits += Math.max(perf.audience - 30, 0); // 음수 일 경우 포인트는 0점이 적립
-    // 희극 관객 5명 마다 추가 포인트를 제공
-    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5); // 소수값 버림
-    // 청구 내역을 출력
-    result += `${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
-    totalAmount += thisAmount;
+    result += `총액 : ${format(totalAmount / 100)}\n`;
+    result += `적립 포인트 : ${volumeCredits}점\n`;
+    return result;
   }
 
-  result += `총액 : ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트 : ${volumeCredits}점\n`;
-  return result;
+  const resultOfStatement = statement(invoiceData, playData);
+  console.log(resultOfStatement);
 }
-
-const resultOfStatement = statement(invoiceData, playData);
-console.log(resultOfStatement);
---
+++ 01_리팩터링-첫-번째-예시/01_리팩터링-첫번째-예시_Tony/1_첫번째-예시/statement_1-4.ts
@@ -0,0 +1,133 @@
+{
+  // 1. 첫 번째 예시
+  const playData: Plays = {
+    hamlet: { name: 'Hamlet', type: 'tragedy' },
+    asLike: { name: 'As You Like It', type: 'comedy' },
+    othello: { name: 'Othello', type: 'tragedy' },
+  };
+
+  const invoiceData: Invoice = {
+    customer: 'BigCo',
+    performances: [
+      {
+        playID: 'hamlet',
+        audience: 55,
+      },
+      {
+        playID: 'asLike',
+        audience: 35,
+      },
+      {
+        playID: 'othello',
+        audience: 40,
+      },
+    ],
+  };
+
+  // 1.4 ~ 1.5 statement() 함수 쪼개기
+
+  type Performance = {
+    playID: 'hamlet' | 'asLike' | 'othello';
+    audience: number;
+  };
+
+  type Invoice = {
+    customer: string;
+    performances: Performance[];
+  };
+
+  type PlayDetail = {
+    name: string;
+    type: 'tragedy' | 'comedy';
+  };
+
+  type Plays = {
+    hamlet: PlayDetail;
+    asLike: PlayDetail;
+    othello: PlayDetail;
+  };
+
+  function statement(invoice: Invoice, plays: Plays) {
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+    for (let perf of invoice.performances) {
+      const play = playFor(plays, perf); // 이건 굳이 함수로 만들어야 되나? plays[perf.playID];
+      const thisAmount = amountFor(perf, play); // 총액
+      // 청구 내역을 출력
+      result += `${play.name}: ${formatAsUSD(thisAmount)} (${
+        perf.audience
+      }석)\n`;
+    }
+
+    result += `총액 : ${formatAsUSD(totalAmount(invoice, plays))}\n`;
+    result += `적립 포인트 : ${totalVolumeCredits(plays, invoice)}점\n`;
+    return result;
+
+    function amountFor(aPerformance: Performance, play: PlayDetail) {
+      let result = 0;
+      switch (play.type) {
+        case 'tragedy':
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case 'comedy':
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${play.type}`);
+      }
+      return result;
+    }
+
+    function playFor(plays: Plays, aPerformance: Performance) {
+      // 책엔 plays를 받지 않지만 typescript에선 받지 않으면 에러이므로 parameter를 추가 함
+      return plays[aPerformance.playID];
+    }
+
+    function volumeCreditsFor(play: PlayDetail, perf: Performance) {
+      // 포인트 적립
+      let result = 0;
+      result += Math.max(perf.audience - 30, 0); // 음수 일 경우 포인트는 0점이 적립
+      // 희극 관객 5명 마다 추가 포인트를 제공
+      if (play.type === 'comedy') result += Math.floor(perf.audience / 5); // 소수값 버림
+      return result;
+    }
+
+    function formatAsUSD(aNumber: number) {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+      }).format(aNumber / 100);
+    }
+
+    function totalVolumeCredits(plays: Plays, invoice: Invoice) {
+      let volumeCredits = 0;
+      for (let perf of invoice.performances) {
+        const play = playFor(plays, perf);
+        // 포인트 적립
+        volumeCredits += volumeCreditsFor(play, perf);
+      }
+      return volumeCredits;
+    }
+
+    function totalAmount(invoice: Invoice, plays: Plays) {
+      let totalAmount = 0;
+      for (let perf of invoice.performances) {
+        const play = playFor(plays, perf); // 이건 굳이 함수로 만들어야 되나? plays[perf.playID];
+        const thisAmount = amountFor(perf, play); // 총액
+        totalAmount += thisAmount;
+      }
+      return totalAmount;
+    }
+  } // statement() 끝
+
+  const resultOfStatement = statement(invoiceData, playData);
+  console.log(resultOfStatement);
+}
