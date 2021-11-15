225508c1de9e2ad7af298c245aa9dddd47ab5a08
TaehwanGo <gth1123@naver.com>
2021-11-15 21:29:46 +0900

    1-6. amountFor 옮기기

--- 01_리팩터링-첫-번째-예시/01_리팩터링-첫번째-예시_Tony/1_첫번째-예시/statement_1-6.ts
+++ 01_리팩터링-첫-번째-예시/01_리팩터링-첫번째-예시_Tony/1_첫번째-예시/statement_1-6.ts
@@ -49,6 +49,7 @@
 
   type StatementPerformance = Performance & {
     play: PlayDetail;
+    amount: number;
   };
 
   type StatementData = {
@@ -56,11 +57,11 @@
     performances: StatementPerformance[]; // Performance[];
   };
 
-  function renderPlainText(statementData: StatementData, plays: Plays) {
+  function renderPlainText(statementData: StatementData) {
     let result = `청구 내역 (고객명: ${statementData.customer})\n`;
     for (let perf of statementData.performances) {
       // const play = playFor(plays, perf); // 이건 굳이 함수로 만들어야 되나? plays[perf.playID];
-      const thisAmount = amountFor(perf); // 총액
+      const thisAmount = perf.amount; // 총액
       // 청구 내역을 출력
       result += `${perf.play.name}: ${formatAsUSD(thisAmount)} (${
         perf.audience
@@ -70,28 +71,6 @@
     result += `총액 : ${formatAsUSD(totalAmount())}\n`;
     result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
     return result;
-
-    function amountFor(aPerformance: StatementPerformance) {
-      let result = 0;
-      switch (aPerformance.play.type) {
-        case 'tragedy':
-          result = 40000;
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
-        case 'comedy':
-          result = 30000;
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-        default:
-          throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-      }
-      return result;
-    }
 
     function volumeCreditsFor(perf: StatementPerformance) {
       // 포인트 적립
@@ -122,7 +101,7 @@
     function totalAmount() {
       let totalAmount = 0;
       for (let perf of statementData.performances) {
-        const thisAmount = amountFor(perf); // 총액
+        const thisAmount = perf.amount; // 총액
         totalAmount += thisAmount;
       }
       return totalAmount;
@@ -135,15 +114,43 @@
       customer: invoice.customer,
       performances: invoice.performances.map(enrichPerformance),
     };
-    return renderPlainText(statementData, plays); // 두 번째 단계 : 청구 내역 출력
+    return renderPlainText(statementData); // 두 번째 단계 : 청구 내역 출력
 
-    function enrichPerformance(aPerformance: Performance) {
-      return { ...aPerformance, play: playFor(aPerformance) }; // Object.assign({}, aPerformance) 와 동일
+    function enrichPerformance(
+      aPerformance: Performance,
+    ): StatementPerformance {
+      const performance = {
+        ...aPerformance,
+        play: playFor(aPerformance),
+      }; // Object.assign({}, aPerformance) 와 동일
+      return { ...performance, amount: amountFor(performance) };
     }
 
     function playFor(aPerformance: Performance) {
       // 책엔 plays를 받지 않지만 typescript에선 받지 않으면 에러이므로 parameter를 추가 함
       return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance: Performance & { play: PlayDetail }) {
+      let result = 0;
+      switch (aPerformance.play.type) {
+        case 'tragedy':
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case 'comedy':
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+      }
+      return result;
     }
   }
 
