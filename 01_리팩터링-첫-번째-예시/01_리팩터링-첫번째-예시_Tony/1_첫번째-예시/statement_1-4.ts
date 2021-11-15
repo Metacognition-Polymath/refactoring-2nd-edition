78f9937e120479463b9d0a8fcafd820bf5cb5639
TaehwanGo <gth1123@naver.com>
2021-11-15 17:53:57 +0900

    1-5. 중간 점검: 난무하는 중첩 함수

--- 01_리팩터링-첫-번째-예시/01_리팩터링-첫번째-예시_Tony/1_첫번째-예시/statement_1-4.ts
+++ 01_리팩터링-첫-번째-예시/01_리팩터링-첫번째-예시_Tony/1_첫번째-예시/statement_1-4.ts
@@ -47,75 +47,7 @@
     othello: PlayDetail;
   };
 
-  function amountFor(aPerformance: Performance, play: PlayDetail) {
-    let result = 0;
-    switch (play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-    return result;
-  }
-
-  function playFor(plays: Plays, aPerformance: Performance) {
-    // 책엔 plays를 받지 않지만 typescript에선 받지 않으면 에러이므로 parameter를 추가 함
-    return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(play: PlayDetail, perf: Performance) {
-    // 포인트 적립
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0); // 음수 일 경우 포인트는 0점이 적립
-    // 희극 관객 5명 마다 추가 포인트를 제공
-    if (play.type === 'comedy') result += Math.floor(perf.audience / 5); // 소수값 버림
-    return result;
-  }
-
-  function formatAsUSD(aNumber: number) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
-
-  function totalVolumeCredits(plays: Plays, invoice: Invoice) {
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-      const play = playFor(plays, perf);
-      // 포인트 적립
-      volumeCredits += volumeCreditsFor(play, perf);
-    }
-    return volumeCredits;
-  }
-
-  function totalAmount(invoice: Invoice, plays: Plays) {
-    let totalAmount = 0;
-    for (let perf of invoice.performances) {
-      const play = playFor(plays, perf); // 이건 굳이 함수로 만들어야 되나? plays[perf.playID];
-      const thisAmount = amountFor(perf, play); // 총액
-      totalAmount += thisAmount;
-    }
-    return totalAmount;
-  }
-
-  function statement(
-    invoice: Invoice,
-    plays: Plays,
-    formatAsUSD: (aNumber: number) => string,
-  ) {
+  function statement(invoice: Invoice, plays: Plays) {
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
@@ -130,8 +62,72 @@
     result += `총액 : ${formatAsUSD(totalAmount(invoice, plays))}\n`;
     result += `적립 포인트 : ${totalVolumeCredits(plays, invoice)}점\n`;
     return result;
-  }
 
-  const resultOfStatement = statement(invoiceData, playData, formatAsUSD);
+    function amountFor(aPerformance: Performance, play: PlayDetail) {
+      let result = 0;
+      switch (play.type) {
+        case 'tragedy':
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case 'comedy':
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${play.type}`);
+      }
+      return result;
+    }
+
+    function playFor(plays: Plays, aPerformance: Performance) {
+      // 책엔 plays를 받지 않지만 typescript에선 받지 않으면 에러이므로 parameter를 추가 함
+      return plays[aPerformance.playID];
+    }
+
+    function volumeCreditsFor(play: PlayDetail, perf: Performance) {
+      // 포인트 적립
+      let result = 0;
+      result += Math.max(perf.audience - 30, 0); // 음수 일 경우 포인트는 0점이 적립
+      // 희극 관객 5명 마다 추가 포인트를 제공
+      if (play.type === 'comedy') result += Math.floor(perf.audience / 5); // 소수값 버림
+      return result;
+    }
+
+    function formatAsUSD(aNumber: number) {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+      }).format(aNumber / 100);
+    }
+
+    function totalVolumeCredits(plays: Plays, invoice: Invoice) {
+      let volumeCredits = 0;
+      for (let perf of invoice.performances) {
+        const play = playFor(plays, perf);
+        // 포인트 적립
+        volumeCredits += volumeCreditsFor(play, perf);
+      }
+      return volumeCredits;
+    }
+
+    function totalAmount(invoice: Invoice, plays: Plays) {
+      let totalAmount = 0;
+      for (let perf of invoice.performances) {
+        const play = playFor(plays, perf); // 이건 굳이 함수로 만들어야 되나? plays[perf.playID];
+        const thisAmount = amountFor(perf, play); // 총액
+        totalAmount += thisAmount;
+      }
+      return totalAmount;
+    }
+  } // statement() 끝
+
+  const resultOfStatement = statement(invoiceData, playData);
   console.log(resultOfStatement);
 }
