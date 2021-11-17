036247bcc862521776c7f44e6ca41ffc40bd0dac
hminn <hmkim0008@gmail.com>
2021-11-17 19:45:40 +0900

    style: Prettier 적용

--- 01_리팩터링-첫-번째-예시/01_리팩터링-첫번째-예시_hyeonmin/srcs/createStatementData.js
+++ 01_리팩터링-첫-번째-예시/01_리팩터링-첫번째-예시_hyeonmin/srcs/createStatementData.js
@@ -8,7 +8,10 @@ function createStatementData(invoice, plays) {
 	return statementData;
 
 	function enrichPerformance(aPerformance) {
-		const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+		const calculator = createPerformanceCalculator(
+			aPerformance,
+			playFor(aPerformance)
+		);
 		const result = Object.assign({}, aPerformance); // 얕은 복사 수행
 		result.play = calculator.play;
 		result.amount = calculator.amount;
@@ -21,39 +24,34 @@ function createStatementData(invoice, plays) {
 	function totalVolumeCredits(data) {
 		return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
 	}
-	function volumeCreditsFor(aPerformance) {
-		return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
-	}
-	function amountFor(aPerformance) {
-		return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-	}
 	function playFor(aPerformance) {
 		return plays[aPerformance.playID];
 	}
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-	switch(aPlay.type){
-		case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
-		case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+	switch (aPlay.type) {
+		case "tragedy":
+			return new TragedyCalculator(aPerformance, aPlay);
+		case "comedy":
+			return new ComedyCalculator(aPerformance, aPlay);
 		default:
 			throw new Error(`알 수 없는 장르: ${aPlay.type}`);
 	}
 }
 
 class PerformanceCalculator {
+	constructor(aPerformance, aPlay) {
+		this.performance = aPerformance;
+		this.play = aPlay;
+	}
+
 	get amount() {
-		throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
+		throw new Error("서브클래스에서 처리하도록 설계되었습니다.");
 	}
 
 	get volumeCredits() {
 		return Math.max(this.performance.audience - 30, 0);
-
-	}
-
-	constructor(aPerformance, aPlay) {
-		this.performance = aPerformance;
-		this.play = aPlay
 	}
 }
 
@@ -74,7 +72,7 @@ class ComedyCalculator extends PerformanceCalculator {
 			result += 10000 + 500 * (this.performance.audience - 20);
 		}
 		result += 300 * this.performance.audience;
-		return result
+		return result;
 	}
 
 	get volumeCredits() {
