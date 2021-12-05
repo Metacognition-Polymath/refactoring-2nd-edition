d52db596910dddbae5eac19e00094f584da506fa
Shin Dahye <64780560+dahye1013@users.noreply.github.com>
2021-12-06 05:02:28 +0900

    SHUZAN/4장 (#19)

---
+++ 04_테스트-구축하기/04_테스트-구축하기_SHUZAN/app/src/Province.js
@@ -0,0 +1,99 @@
+export default class Province {
+  constructor(doc) {
+    this._name = doc._name;
+    this._producers = [];
+    this._totalProduction = 0;
+    this._demand = doc.demand;
+    this._price = doc.price;
+    doc.producers.forEach((d) => {
+      this.addProducer(new Producer(this, d));
+    });
+  }
+
+  get name() {
+    return this._name;
+  }
+  get producers() {
+    return this._producers !== "" ? this._producers : [];
+  }
+  get totalProduction() {
+    return this._totalProduction;
+  }
+  set totalProduction(arg) {
+    this._totalProduction = arg;
+  }
+  get demand() {
+    return this._demand;
+  }
+  set demand(arg) {
+    this._demand = parseInt(arg);
+  }
+  get price() {
+    return this._price;
+  }
+  set price(arg) {
+    this._price = parseInt(arg);
+  }
+
+  addProducer(arg) {
+    this._producers.push(arg);
+    this._totalProduction += arg.production;
+  }
+
+  get shortfall() {
+    return this._demand - this.totalProduction;
+  }
+
+  get profit() {
+    return this.demandValue - this.demandCost;
+  }
+
+  get demandValue() {
+    return this.satisfiedDemand * this.price;
+  }
+
+  get satisfiedDemand() {
+    return Math.min(this._demand, this.totalProduction);
+  }
+
+  get demandCost() {
+    let remainingDemand = this.demand;
+    let result = 0;
+    this.producers
+      .sort((a, b) => a.cost - b.cost)
+      .forEach((p) => {
+        const contribution = Math.min(remainingDemand, p.production);
+        remainingDemand -= contribution;
+        result += contribution * p.cost;
+      });
+    return result;
+  }
+}
+
+class Producer {
+  constructor(aProvince, data) {
+    this._province = aProvince;
+    this._cost = data.cost;
+    this._name = data.name;
+    this._production = data.production || 0;
+  }
+  get name() {
+    return this._name;
+  }
+  get cost() {
+    return this._cost;
+  }
+  set cost(arg) {
+    this._cost = parseInt(arg);
+  }
+
+  get production() {
+    return this._production;
+  }
+  set production(amountStr) {
+    const amount = parseInt(amountStr);
+    const newProduction = Number.isNaN(amount) ? 0 : amount;
+    this._province.totalProduction += newProduction - this._production;
+    this._production = newProduction;
+  }
+}
