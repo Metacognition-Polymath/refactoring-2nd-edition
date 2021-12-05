9373027112f1b14730a8a72048ed87d254f45219
고태환 <gotaehwan@gotaehwans-MacBook-Pro.local>
2021-12-05 16:09:39 +0900

    첫번째 테스트

--- 04_테스트-구축하기/04_테스트-구축하기_tony/4장.test.ts
+++ 04_테스트-구축하기/04_테스트-구축하기_tony/4장.test.ts
@@ -3,6 +3,6 @@ import { Province, sampleProvinceData } from "./Province";
 describe("province", function () {
   it("shorfall", function () {
     const asia = new Province(sampleProvinceData());
-    expect(3).toBe(3);
+    expect(5).toBe(asia.shortfall);
   });
 });
