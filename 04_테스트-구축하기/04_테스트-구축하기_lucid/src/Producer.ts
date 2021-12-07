64c05b45dc2a309048575c3031e1f0fe74b8287e
goodman-92 <whe1915@gmail.com>
2021-12-07 22:10:55 +0900

    charter 04 - 테스트할 샘플 코드 작성 및 환경구축

---
+++ 04_테스트-구축하기/04_테스트-구축하기_lucid/src/Producer.ts
@@ -0,0 +1,37 @@
+import Province from "./Province";
+
+interface IProducer {
+  name: string;
+  cost: number;
+  province: Province;
+  production: number;
+}
+
+class Producer implements IProducer {
+  private readonly _name: string;
+  province: Province;
+
+  private _cost :number;
+  private _production: number;
+
+  constructor(province: Province, data: any) {
+    this._name = data.name
+    this._cost = data.cost;
+    this._production = data.production || 0;
+    this.province = province;
+  }
+
+  get name(){ return this._name }
+  get cost(){ return this._cost }
+  set cost(arg) { this._cost = arg }
+  get production(){ return this._production }
+  set production(arg){
+    const newProduction = Number.isNaN(arg) ?  0: arg;
+    this.province.totalProduction += newProduction - this._production;
+    this._production = newProduction;
+  }
+  
+}
+
+
+export default Producer;
