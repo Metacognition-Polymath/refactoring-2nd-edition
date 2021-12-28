db02883386b19e28acca49682501461f6fae1c08
hoin <72450781+lucid-jin@users.noreply.github.com>
2021-12-28 14:46:42 +0900

    charter 08. Account 클래스 함수 위임 메소드로 리펙토링

--- 08_기능-이동/08_기능이동_lucid/main.js
+++ 08_기능-이동/08_기능이동_lucid/main.js
@@ -1,21 +1,34 @@
 class Account {
 	get bankCharge(){
 		let result = 4.5;
-		if (this._daysOverdrawn > 0) result +=this.overdraftCharge;
+		if (this._daysOverdrawn > 0) result += this.overdraftCharge;
 		return result;
 	}
-	get overdraftCharge(){
-		if (this.type._isPremium){
-			const baseCharge = 10;
-			if (this.daysOverdrawn <= 7)
-				return baseCharge;
-			else
-				return baseCharge + (this.daysOverdrawn -7) * 0.85;
-		}
-		else
-			return this.daysOverdrawn * 1.75;
+	get overdraftCharge() { // 위임 메소드
+		return this.type.overedraftCharge(this.daysOverdrawn)
 	}
 }
 
 
-// overdraftCharge 를 계좌 종류 클래스인 AccountType 으로 옮기느게 자연스럽다
+// TODO::overdraftCharge 를 계좌 종류 클래스인 AccountType 으로 옮기느게 자연스럽다
+class AccountType {
+	get bankCharge() {
+		let result  = 4.5;
+		if (this._daysOverdrwawn > 0)
+			result += this.type.overdraftCharge(this.daysOverdrawn);
+		return result;
+	}
+	overdraftCharge(account){
+		if (this.isPremium){
+			const baseCharge = 10;
+			if (account.daysOverdrawn <= 7)
+				return baseCharge;
+			else
+				return baseCharge + (account.daysOverdrawn - 7) * 0.85;
+		}
+		else
+			return account.daysOverdrawn * 1.75;
+	}
+}
+
+// 
