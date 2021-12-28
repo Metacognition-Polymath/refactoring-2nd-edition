9fd56c86bcee836c4d9213122fdd50432e9bc505
hoin <72450781+lucid-jin@users.noreply.github.com>
2021-12-28 15:10:52 +0900

    charter 08.2 기능이동 절차 및 원본 클래스 생성

--- 08_기능-이동/08_기능이동_lucid/main.js
+++ 08_기능-이동/08_기능이동_lucid/main.js
@@ -1,34 +1,21 @@
-class Account {
-	get bankCharge(){
-		let result = 4.5;
-		if (this._daysOverdrawn > 0) result += this.overdraftCharge;
-		return result;
-	}
-	get overdraftCharge() { // 위임 메소드
-		return this.type.overedraftCharge(this.daysOverdrawn)
+class CustomerContract {
+	constructor(startDate) {
+		this._startDate = startDate;
 	}
 }
 
-
-// TODO::overdraftCharge 를 계좌 종류 클래스인 AccountType 으로 옮기느게 자연스럽다
-class AccountType {
-	get bankCharge() {
-		let result  = 4.5;
-		if (this._daysOverdrwawn > 0)
-			result += this.type.overdraftCharge(this.daysOverdrawn);
-		return result;
+class Customer {
+	constructor(name, discountRate) {
+		this._name = name;
+		this._discountRate = discountRate;
+		this._contract = new CustomerContract(dateToday())
 	}
-	overdraftCharge(account){
-		if (this.isPremium){
-			const baseCharge = 10;
-			if (account.daysOverdrawn <= 7)
-				return baseCharge;
-			else
-				return baseCharge + (account.daysOverdrawn - 7) * 0.85;
-		}
-		else
-			return account.daysOverdrawn * 1.75;
+	get discountRate() { return this._discountRate; }
+	becomePreferred(){
+		this._discountRate += 0.03;
+		// 남은일들
+	}
+	applyDiscount(amount){
+		return amount.subtract(amount.multiply(this._discountRate));
 	}
 }
-
-// 
