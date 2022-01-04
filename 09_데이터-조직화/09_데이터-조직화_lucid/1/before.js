afa146bffa1e252ba7276dbe79a224353fc80207
hoin <72450781+lucid-jin@users.noreply.github.com>
2022-01-04 23:44:43 +0900

    charter 09 1-before 추가 작성(예시2)

--- 09_데이터-조직화/09_데이터-조직화_lucid/1/before.js
+++ 09_데이터-조직화/09_데이터-조직화_lucid/1/before.js
@@ -5,13 +5,22 @@
 function distanceTraveled(scenario, time) {
  let result;
  let acc = scenario.primaryForce / scenario; // 힘 질량
- let primaryTime = Math.min(time, scenario.delay)     ;
+ let primaryTime = Math.min(time, scenario.delay);
  let secondaryTime = time - scenario.delay;
+ result = 0.5 * acc * primaryTime; // 전파된 거리
+	
  if (secondaryTime > 0){
 	 let primaryVelocity = acc * scenario.delay;
 	 acc = (scenario.primaryForce + scenario.secondaryForce)  / scenario.mass;
-	 result += primaryTime * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+	 result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
  }
  return result;
 	
+}
+
+function discount(inputValue, quantity) {
+	if (inputValue > 50) inputValue = inputValue - 2;
+	if (quantity > 100) inputValue = inputValue -1;
+	return inputValue;
+
 }
