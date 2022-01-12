0c1a551e2df0decbe0e95e28faa5bbab7a24e9eb
hminn <hmkim0008@gmail.com>
2022-01-12 18:35:04 +0900

    10-4-1. 조건부 로직을 다형성으로 바꾸기 : (1) Bird 클래스 생성 및 각 정보에 대한 조건문을 게터로 만들기

--- 10_조건부-로직-간소화/10_조건부-로직-간소화_Hyeonmin/src/04-1.js
+++ 10_조건부-로직-간소화/10_조건부-로직-간소화_Hyeonmin/src/04-1.js
@@ -1,29 +1,43 @@
-const feathers = birds => new Map(birds.map(b => [b.name, feather(b)]))
-const velocities = birds => new Map(birds.map(b => [b.name, velocity(b)]))
+const feathers = birds => new Map(birds.map(b => {
+	const bird = new Bird(b);
+	return [b.name, bird.feather];
+}))
 
-const feather = bird => {
-  switch (bird.type) {
-    case '유럽 제비':
-      return '보통'
-    case '아프리카 제비':
-      return bird.numberOfCoconuts > 2 ? '지침' : '보통'
-    case '노르웨이 파랑 앵무':
-      return bird.voltage > 100 ? '그을림' : '예쁨'
-    default:
-      return '알수없음'
-  }
-}
-const velocity = bird => {
-  switch (bird.type) {
-    case '유럽 제비':
-      return 35
-    case '아프리카 제비':
-      return 40 - 2 * bird.numberOfCoconuts
-    case '노르웨이 파랑 앵무':
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10
-    default:
-      return null
-  }
+const velocities = birds => new Map(birds.map(b => {
+	const bird = new Bird(b);
+	return [b.name, bird.velocity];
+}))
+
+class Bird {
+	constructor(birdObject) {
+		Object.assign(this, birdObject);
+	}
+
+	get feather() {
+		switch (this.type) {
+			case '유럽 제비':
+				return '보통'
+			case '아프리카 제비':
+				return this.numberOfCoconuts > 2 ? '지침' : '보통'
+			case '노르웨이 파랑 앵무':
+				return this.voltage > 100 ? '그을림' : '예쁨'
+			default:
+				return '알수없음'
+		}
+	}
+
+	get velocity() {
+		switch (this.type) {
+			case '유럽 제비':
+				return 35
+			case '아프리카 제비':
+				return 40 - 2 * this.numberOfCoconuts
+			case '노르웨이 파랑 앵무':
+				return this.isNailed ? 0 : 10 + this.voltage / 10
+			default:
+				return null
+		}
+	}
 }
 
 const birds = [
