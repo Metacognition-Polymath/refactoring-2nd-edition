ed83a26da4db0a10310e1d44d33dd244ff462885
hminn <hmkim0008@gmail.com>
2022-01-12 17:55:58 +0900

    10-3-1. 중첩 조건문을 보호 구문으로 바꾸기 : (2) 예외 케이스들을 모두 보호 구문으로 처리

--- 10_조건부-로직-간소화/10_조건부-로직-간소화_Hyeonmin/src/03-1-after.js
+++ 10_조건부-로직-간소화/10_조건부-로직-간소화_Hyeonmin/src/03-1-after.js
@@ -1,10 +1,11 @@
 const payAmount = employee => {
-  let result;
+	// 가변 변수 제거하기
 
+	// 예외 케이스들은 모두 보호 구문으로 처리
 	if (employee.isSeperated) return { amount: 0, reasonCode: 'SEP' };
-  if (employee.isRetired) result = { amount: 0, reasonCode: 'RET' }
-  else {
-    result = { amount: 100, reasonCode: 'WRK' }
-  }
-  return result
+	if (employee.isRetired) return { amount: 0, reasonCode: 'RET' };
+
+	// 급여 계산 로직 처리
+	const result = { amount: 100, reasonCode: 'WRK' };
+	return result;
 }
