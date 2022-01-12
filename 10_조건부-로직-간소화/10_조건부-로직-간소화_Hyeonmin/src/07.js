3cfed9893f082faa8cca58a097c36c8d6a97d32b
hminn <hmkim0008@gmail.com>
2022-01-12 20:13:12 +0900

    10-7. 제어 플래그를 탈출문으로 바꾸기 : after 코드

--- 10_조건부-로직-간소화/10_조건부-로직-간소화_Hyeonmin/src/07.js
+++ 10_조건부-로직-간소화/10_조건부-로직-간소화_Hyeonmin/src/07.js
@@ -1,17 +1,14 @@
 const sendAlert = () => console.warn("악당을 찾았소");
 
 const checkForMiscreants = (people) => {
-	let found = false;
 	for (const p of people) {
-		if (!found) {
-			if (p === "조커") {
-				sendAlert();
-				found = true;
-			}
-			if (p === "사루만") {
-				sendAlert();
-				found = true;
-			}
+		if (p === "조커") {
+			sendAlert();
+			return ;
+		}
+		if (p === "사루만") {
+			sendAlert();
+			return ;
 		}
 	}
 };
