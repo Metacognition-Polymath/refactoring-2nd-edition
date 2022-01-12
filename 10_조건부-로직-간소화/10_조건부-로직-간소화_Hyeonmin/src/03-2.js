51164d2f95bc13323e9e68af8b3cd967e83f452f
hminn <hmkim0008@gmail.com>
2022-01-12 18:05:26 +0900

    10-3-2. 중첩 조건문을 보호 구문으로 바꾸기 - 조건 반대로 만들기 : after 코드

--- 10_조건부-로직-간소화/10_조건부-로직-간소화_Hyeonmin/src/03-2.js
+++ 10_조건부-로직-간소화/10_조건부-로직-간소화_Hyeonmin/src/03-2.js
@@ -1,9 +1,14 @@
 const adjustCapital = anInstrument => {
-  let result = 0
-  if (anInstrument.capital > 0) {
-    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-      result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor
-    }
-  }
-  return result
+	/*
+	1. 바깥쪽 조건문부터 조건을 역으로 설정하여 추출, 보호 구문으로.
+	2. 예외 케이스들을 모두 보호 구문으로 변경
+	3. 동일한 결과를 반환하므로, 조건문 통합 적용
+	*/
+
+	if ((anInstrument.capital <= 0)
+			|| (anInstrument.interestRate <= 0)
+			|| (anInstrument.duration <= 0)) return 0;
+
+	const result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+	return result;
 }
