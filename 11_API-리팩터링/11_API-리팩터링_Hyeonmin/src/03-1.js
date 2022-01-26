45fa317ab38f39386bee10f2dc52f9aea98cd442
hminn <hmkim0008@gmail.com>
2022-01-26 19:12:44 +0900

    11.3(1): 조건문 분해하기 적용
    - 새로 만든 두 함수가 호출자의 의도를 더 잘 드러낸다.

--- 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/03-1.js
+++ 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/03-1.js
@@ -1,4 +1,7 @@
 /*
+- 배송일자를 계산하는 함수에 대한 예시
+- isRush에 따라 호출되는 함수가 실행할 로직이 변경 => 전형적인 플래그 인수
+- 호출하는 쪽에서 넘겨주는 불리언 값이 무엇인지 명확하지 못하다.
  */
 
 class Place {
@@ -16,20 +19,25 @@ class Order {
 		this.placedOn = new Place();
 	}
 }
+
+const rushDeliveryDate = (anOrder) => {
+	let deliveryTime;
+	if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+	else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+	else deliveryTime = 3;
+	return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+const regularDeliveryDate = (anOrder) => {
+	let deliveryTime;
+	if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+	else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+	else deliveryTime = 4;
+	return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
 const deliveryDate = (anOrder, isRush) => {
-	if (isRush) {
-		let deliveryTime;
-		if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-		else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
-		else deliveryTime = 3;
-		return anOrder.placedOn.plusDays(1 + deliveryTime);
-	} else {
-		let deliveryTime;
-		if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-		else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
-		else deliveryTime = 4;
-		return anOrder.placedOn.plusDays(2 + deliveryTime);
-	}
+	if (isRush) return rushDeliveryDate(anOrder);
+	else return regularDeliveryDate(anOrder);
 };
 
 console.log(deliveryDate(new Order("MA"), true));
