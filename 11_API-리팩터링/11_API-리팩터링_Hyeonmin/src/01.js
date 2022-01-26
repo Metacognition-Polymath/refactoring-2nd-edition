87b72b0035e5fabf329a6c527500bd12ce5c030c
hminn <hmkim0008@gmail.com>
2022-01-26 18:41:02 +0900

    11.1: 리팩터링 완료
    - 새로운 질의 함수를 호출하도록 수정
    - 변경 함수에서 질의 관련 코드 제거

--- 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/01.js
+++ 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/01.js
@@ -1,32 +1,15 @@
 const setOffAlarms = () => {
-  console.warn('악당을 찾았소')
+  console.warn('악당을 찾았소');
 }
+
+const findMiscreant = people => people.find(p => p === '조커' || p === '사루만');
 const alertForMiscreant = people => {
-  for (const p of people) {
-    if (p === '조커') {
-      setOffAlarms()
-      return '조커'
-    }
-    if (p === '사루만') {
-      setOffAlarms()
-      return '사루만'
-    }
+  if (findMiscreant(people)) {
+    setOffAlarms();
   }
-  return ''
 }
 
-const findMisreant = people => {
-  for (const p of people) {
-    if (p === '조커') {
-      setOffAlarms()
-      return '조커'
-    }
-    if (p === '사루만') {
-      setOffAlarms()
-      return '사루만'
-    }
-  }
-  return ''
-}
-
-alertForMiscreant(['슈퍼맨', '배트맨', '아이언맨', '사루만', '블랙위도우', '조커', '스파이더맨'])
+const people = ['슈퍼맨', '배트맨', '아이언맨', '사루만', '블랙위도우', '조커', '스파이더맨'];
+// const people = ['슈퍼맨', '배트맨', '아이언맨', '블랙위도우', '스파이더맨'];
+const found = findMiscreant(people);
+alertForMiscreant(people);
