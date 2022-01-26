fba205bddd943a293e6f6d66e74cf90e97ebc119
hminn <hmkim0008@gmail.com>
2022-01-26 18:48:27 +0900

    11.2(1): after

--- 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/02-1.js
+++ 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/02-1.js
@@ -1,6 +1,3 @@
-const tenPercentRaise = person => {
-  person.salary = person.salary.multiply(1.1)
-}
-const fivePercentRaise = person => {
-  person.salary = person.salary.multiply(1.05)
+const raise = (aPerson, factor) => {
+	aPerson.salary = aPerson.salary.multiply(1 + factor);
 }
