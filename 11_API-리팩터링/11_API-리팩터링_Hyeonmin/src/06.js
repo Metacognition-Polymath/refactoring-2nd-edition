d88bf778f5fac4ea7ce4ac0c7cb7852a35102a56
hminn <hmkim0008@gmail.com>
2022-01-26 20:19:26 +0900

    11.6: 변수 추출하기 및 메서드로 추출하기 적용

--- 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/06.js
+++ 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/06.js
@@ -1,3 +1,10 @@
+/*
+- 실내온도 제어 시스템: 사용자는 온도조절기로 온도 설정 가능, 목표 온도는 난방 계획에서 정한 범위에서만 선택.
+- targetTemperature() 메서드가 전역 객체인 thermostat에 의존하는 것이 신경쓰임
+- 그러니, 이 전역 객체에 건네는 질의 메서드를 매개변수로 옮겨서 의존성을 끊어보자.
+-
+*/
+
 const thermostat = {
 	selectedTemperature: 25,
 	currentTemperature: 27,
@@ -7,9 +14,14 @@ class HeatingPlan {
 	#max;
 	#min;
 	get targetTemperature() {
-		if (thermostat.selectedTemperature > this.#max) return this.#max;
-		else if (thermostat.selectedTemperature < this.#min) return this.#min;
-		else return thermostat.selectedTemperature;
+		const selectedTemperature = thermostat.selectedTemperature;
+		return this.NEWtargetTemperature(selectedTemperature);
+	}
+
+	NEWtargetTemperature(selectedTemperature) {
+		if (selectedTemperature > this.#max) return this.#max;
+		else if (selectedTemperature < this.#min) return this.#min;
+		else return selectedTemperature;
 	}
 }
 
