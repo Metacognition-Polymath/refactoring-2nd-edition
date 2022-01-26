95fb973d4c914cfcc7834b50a49ae6baa41dc2e0
hminn <hmkim0008@gmail.com>
2022-01-26 18:54:25 +0900

    11.2(2): middleBand를 기준으로 매개변수화 함수 구성
    - 매개변수 고려 : 상한, 하한
    - 함수 선언 바꾸기 적용
    - 함수 이름 바꾸기 적용

--- 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/02-2.js
+++ 11_API-리팩터링/11_API-리팩터링_Hyeonmin/src/02-2.js
@@ -1,3 +1,8 @@
+/*
+- 대역(band)을 다루는 세 함수의 로직이 상당히 비슷하므로, 매개변수화 함수로 통합
+- 통합 시에는, 먼저 대상 함수 중 하나를 골라 매개변수를 추가. (다른 함수들까지 고려)
+*/
+
 const usd = aNumber =>
   new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -5,11 +10,15 @@ const usd = aNumber =>
     minimumFractionDigits: 2,
   }).format(aNumber / 100)
 
+
+const withinBand = (usage, bottom, top) => {
+	return (usage > 100 ? Math.min(usage, 200) - 100 : 0);
+}
 const bottomBand = usage => Math.min(usage, 100)
 const middleBand = usage => (usage > 100 ? Math.min(usage, 200) - 100 : 0)
 const topBand = usage => (usage > 200 ? usage - 200 : 0)
 const baseCharge0 = usage => {
   if (usage < 0) return usd(0)
-  const amount = bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07
+  const amount = bottomBand(usage) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07
   return usd(amount)
 }
